bfd8d16fdc08e803ca2e83646b8bf7c9cd35f322
Yassine ANZAR BASHA <anzarb_y@piscine-mars-a>
2024-04-15 15:41:13 +0000

    feat: link django to the mysql db

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_bubble_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
