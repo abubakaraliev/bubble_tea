e8ce7dd080cf1326c410769bc81cc080ce048888
aliev_a <aliev_a@etna-alternance.net>
2024-04-16 14:17:06 +0200

    add: register view & controller

--- app/BubbleTea/views.py
+++ app/BubbleTea/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-
+from .models import *
+from .forms import CreateUserForm
 
 def home_page(request):
-    print(request.headers)
+    # print(request.headers)
     return render(request, "index.html", {})
 
 def login_page(request):
-    print(request.headers)
+    # print(request.headers)
     return render(request, "login.html", {})
 
 def register_page(request):
-    print(request.headers)
-    return render(request, "register.html", {})
+    # print(request.headers)
+    form = CreateUserForm()
+    
+    if  request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    context = {'form':form}
+    return render(request, "register.html", context)
 
 def profile_page(request):
-    print(request.headers)
+    # print(request.headers)
     return render(request, "profile.html", {})
