89bb016394da96c73372d09c50c9bfa70db5ab26
aliev_a <aliev_a@etna-alternance.net>
2024-04-19 14:15:13 +0200

    fix login

--- app/BubbleTea/views.py
+++ app/BubbleTea/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db import connection
 from .forms import LoginUserForm, CreateUserForm, AccountUserForm ,ProfileUserForm
+import bcrypt
 import jwt
 
 def home(request):
@@ -19,10 +20,12 @@ def register(request):
                 email = form.cleaned_data.get('email')
                 password = form.cleaned_data.get('password')
 
+                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
                 with connection.cursor() as cursor:
                     cursor.execute(
                         "INSERT INTO Users(username, email, password) VALUES (%s, %s, %s)",
-                        [username, email, password]
+                        [username, email, hashed_password.decode('utf-8')]
                     )
                 
                 messages.success(request, f'Account created for {username}')
@@ -30,11 +33,10 @@ def register(request):
             
             else:
                 form = CreateUserForm()
-                messages.info(request, 'Wrong inputs !')
+                messages.info(request, 'Wrong inputs!')
                 
         except Exception:
             messages.error(request, 'An error occurred.')
-
 
     context = {'form': form}
     return render(request, "register.html", context)
@@ -44,16 +46,19 @@ def login(request):
 
     if request.method == 'POST':
         form = LoginUserForm(data=request.POST)
-        try:
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            
+            print(username, password)
 
-                with connection.cursor() as cursor:
-                    cursor.execute("SELECT * FROM Users WHERE username = %s", [username])
-                    user = cursor.fetchone()
-                
-                if user:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM Users WHERE username = %s", [username])
+                user = cursor.fetchone()
+            print(user)    
+            if user:
+                hashed_password = user[1]
+                if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
                     payload = {'username': username}
                     secret_key = "secret"
                     algorithm = "HS256"
@@ -65,15 +70,13 @@ def login(request):
                     response = redirect('profile')
                     response.set_cookie('access_token', encoded_token)
                     return response
-                
                 else:
-                    messages.error(request, 'User not found!')
-                    
+                    messages.error(request, 'Incorrect password!')
             else:
-                messages.error(request, 'Invalid form submission!')
+                messages.error(request, 'Incorrect username or password!')
                 
-        except Exception:
-            messages.error(request, 'An error occurred.')
+        else:
+            messages.error(request, 'Invalid form submission!')
 
     context = {'form': form}
     return render(request, "login.html", context)
